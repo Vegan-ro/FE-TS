eab850de87084159571486a30a2568a8875ebc0d
Saeyoung Choi <effysogood@gmail.com>
2024-08-11 22:51:00 +0900

    feat: Loading 컴포넌트

---
+++ src/components/Loading/Loading.styles.ts
@@ -0,0 +1,43 @@
+import { keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
+
+const MoveUpDown = keyframes`
+    0% {
+        -webkit-transform:translateY(0)
+    }
+
+    50% {
+        -webkit-transform: translateY(-10%);
+        transform:translateY(-10%)
+    }
+
+    100% {
+        -webkit-transform: translateY(0);
+        transform:translateY(0)
+    }
+`;
+
+export const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(240, 240, 240, 0.4);
+  backdrop-filter: blur(2px);
+  -webkit-backdrop-filter: blur(1px);
+  z-index: 9999;
+`;
+
+export const LogoContainer = styled.div`
+  width: 180px;
+  height: 180px;
+  background-image: url('https://storage.googleapis.com/elice_04/vegan-ro-img/logo.png');
+  bakcground-position: center center;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  animation: ${MoveUpDown} 1s ease-in-out infinite;
+`;
