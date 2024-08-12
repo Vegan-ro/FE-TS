8c11082a73077cfde255a4f74c9b4b79c1eda683
yjinn0719 <yjinn0719@gmail.com>
2024-08-12 17:00:48 +0900

    feat: MenuTag 컴포넌트

---
+++ src/components/MenuTag/MenuTag.styles.ts
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+import { MenuTagBtnProps } from '@/components/MenuTag/MenuTag.types';
+
+export const MenuTagBtn = styled.button<MenuTagBtnProps>`
+  display: flex;
+  height: 50px;
+  width: 100%;
+  max-width: 218px;
+  padding: 12px 16px;
+  justify-content: center;
+  align-items: center;
+  flex: 1 0 0;
+  border-radius: 4px;
+  background: ${(props) => (props.$clicked ? '#4F83371A' : props.theme.colors.gray[10])};
+  border: 1px solid ${(props) => (props.$clicked ? props.theme.colors.green[500] : props.theme.colors.gray[10])};
+`;
+
+export const MenuTagContent = styled.p<MenuTagBtnProps>`
+  color: ${(props) => (props.$clicked ? props.theme.colors.green[500] : props.theme.colors.gray[600])};
+  font-weight: 500;
+  ${(props) => props.theme.media.desktop`
+    font-size: 16px;
+  `}
+  ${(props) => props.theme.media.mobile`
+    font-size: 14px;
+  `}
+`;
