dad2414cb09de1a7c774934f63eb0999524e8be8
parkjiwonnn <jiwonp05@gmail.com>
2024-08-25 22:16:39 +0900

    feat: 라우터 연결, Admin 페이지

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { PATH } from '@/constants/paths';
 
 import { MainPage } from '@/features/Main';
+import { AdminPage } from '@/features/Admin';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={PATH.ROOT} element={<MainPage />} />
+        <Route path={PATH.ADMIN} element={<AdminPage />} />
       </Routes>
     </BrowserRouter>
   );
