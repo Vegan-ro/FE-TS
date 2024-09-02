3743aa639d697c5714bd7e5e969dc77a599720d4
parkjiwonnn <jiwonp05@gmail.com>
2024-09-02 17:47:00 +0900

    feat: adminPlaceAPI

---
+++ src/api/adminPlaceAPI/adminPlaceAPI.types.ts
@@ -0,0 +1,15 @@
+export interface PlaceData {
+  _id: string;
+  name: string;
+  category: string;
+  // category_img: string;
+  vegan_option: boolean;
+  tel: string;
+  address: string;
+  // address_lot_number: string;
+  address_detail: string;
+  // location: number[];
+  open_times: string[];
+  sns_url: string[];
+  user_id?: string;
+}
